3ec60f92d35385ef2f0227b9adbb3f4496401f9b
LuisLopez7 <167396155+LuisLopez7@users.noreply.github.com>
2024-08-07 21:49:27 -0600

    Create user.js

---
+++ frontend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
+});
+
+module.exports = mongoose.models.user || mongoose.model('user', UserSchema);
