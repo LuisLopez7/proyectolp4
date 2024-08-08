2ffe57289ffc38b04ca51d8e885c707f06b0880d
LuisLopez7 <167396155+LuisLopez7@users.noreply.github.com>
2024-08-07 22:20:28 -0600

    Delete instalacion para chart.js

--- instalacion para chart.js
+++
@@ -1,5 +0,0 @@
-para que en las estadisticas y el chart,js les ejecute correctamente instalar esto desde el cmd
-ejemplo selecionar cd C:\proyectolp4
-abre la ruta PS C:\proyectolp4> npm install chart.js
-
-y se instala
